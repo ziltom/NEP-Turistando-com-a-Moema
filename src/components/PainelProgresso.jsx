b042f4d36f9fc16d937cb7a1f4c52ef3868c2ba8
ziltom <ziltommachado@hotmail.com>
2026-09-07 14:09:24 -0300

    atualizacao

--- src/components/PainelProgresso.jsx
+++ src/components/PainelProgresso.jsx
@@ -11,7 +11,7 @@ export default function PainelProgresso({ pontos, descobertas, aoReiniciar }) {
 
   return (
     <section>
-      <h1>Caca a Moema</h1>
+      <h1>Caça à Moema</h1>
 
       <div className="contador">
         <strong>{descobertas.length}</strong> / {pontos.length} encontradas
